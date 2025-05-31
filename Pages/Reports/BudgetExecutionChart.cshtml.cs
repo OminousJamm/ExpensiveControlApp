23fed3b4df6fd94d42097c2a0c5ef9cee7caa5bb
Julian Moyano <51031164+OminousJamm@users.noreply.github.com>
2025-05-30 23:22:21 -0500

    Interface Update

--- Pages/Reports/BudgetExecutionChart.cshtml.cs
+++ Pages/Reports/BudgetExecutionChart.cshtml.cs
@@ -1,3 +1,5 @@
+using ExpensiveControlApp.Services.Interfaces;
+using ExpensiveControlApp.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,24 @@ namespace ExpensiveControlApp.Pages.Reports
 {
     public class BudgetExecutionChartModel : PageModel
     {
-        public void OnGet()
+        private readonly IReportService _reportService;
+
+        public BudgetExecutionChartModel(IReportService reportService)
         {
+            _reportService = reportService;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime From { get; set; } = DateTime.Today.AddMonths(-1);
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime To { get; set; } = DateTime.Today;
+
+        public List<BudgetExecutionDto> ChartData { get; set; } = new();
+
+        public async Task OnGetAsync()
+        {
+            ChartData = await _reportService.GetBudgetExecutionAsync(From, To);
         }
     }
 }
